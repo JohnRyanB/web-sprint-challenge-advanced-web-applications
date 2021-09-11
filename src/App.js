fea15d54c0b35dae7a34df86e1a9aeba4c36fc3d
John-Blackburn <JRBbusinesscontact@gmail.com>
2021-09-11 14:19:07 -0400

    Login Component Funtional

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Redirect, Route } from "react-router-dom";
+import BubblePage from "./components/BubblePage";
 
 import Login from "./components/Login";
 import "./styles.scss";
 
 function App() {
-  return (
-    <Router>
-      <div className="App">
-        <header>
-          Color Picker Sprint Challenge
-          <a data-testid="logoutButton" href="#">logout</a>
-        </header>
-      </div>
-    </Router>
-  );
+	return (
+		<Router>
+			<div className="App">
+				<header>
+					Color Picker Sprint Challenge
+					<a data-testid="logoutButton" href="#">
+						logout
+					</a>
+				</header>
+				<Route exact path="/login" component={Login} />
+				<Route exact path="/" render={() => <Redirect to="/login" />} />
+				<Route exact path="/bubblepage" component={BubblePage} />
+			</div>
+		</Router>
+	);
 }
 
 export default App;
 
 //Task List:
-//1. Add in two routes that link to the Login Component, one for the default path '/' and one for the '/login'.
+//1. Add in two routes that link to the Login Component, one for the default path '/' and one for the '/login'.*
 //2. Render BubblePage as a PrivateRoute
 //2. Build the logout button to call the logout endpoint, remove the localStorage Item and redirect to the login page.
