7321f909af29f75a38af7987dad2c451b5160db4
John-Blackburn <JRBbusinesscontact@gmail.com>
2021-09-12 15:17:06 -0400

    MVP Complete

--- src/components/BubblePage.test.js
+++ src/components/BubblePage.test.js
@@ -1,13 +1,66 @@
-import React from 'react';
-import MutationObserver from 'mutationobserver-shim';
+import React from "react";
+import MutationObserver from "mutationobserver-shim";
+import { render, screen } from "@testing-library/react";
+import BubblePage from "./BubblePage";
+import axiosWithAuth from "../helpers/axiosWithAuth";
+import ColorList from "./ColorList";
 
-import { render, screen} from "@testing-library/react";
-import BubblePage from './BubblePage';
+const getColor = () => {
+	axiosWithAuth()
+		.get("/colors")
+		.then((res) => res.data)
+		.catch((err) => console.log("ERROR: ", err));
+};
 
-test("Renders without errors", ()=> {
-    
+const color = [
+	{
+		color: "bisque",
+		code: {
+			hex: "#dd9a99",
+		},
+		id: 7,
+	},
+	{
+		color: "softyellow",
+		code: {
+			hex: "#dcdd99",
+		},
+		id: 8,
+	},
+	{
+		color: "blanchedalmond",
+		code: {
+			hex: "#ffebcd",
+		},
+		id: 9,
+	},
+	{
+		color: "blue",
+		code: {
+			hex: "#6093ca",
+		},
+		id: 10,
+	},
+	{
+		color: "blueviolet",
+		code: {
+			hex: "#8a2be2",
+		},
+		id: 11,
+	},
+];
+
+const mockGetColors = getColor();
+jest.mock(mockGetColors);
+
+test("Renders without errors", () => {
+	render(<BubblePage />);
 });
 
-test("Renders appropriate number of colors passed in through mock", async ()=> {
-    //Keep in mind that our service is called on mount for this component.
+test("Renders appropriate number of colors passed in through mock", async () => {
+	render(<BubblePage />);
+	render(<ColorList colors={color} />);
+
+	expect(screen.getAllByTestId("color")).toHaveLength(5);
+	//Keep in mind that our service is called on mount for this component.
 });
--- src/components/ColorList.test.js
+++ src/components/ColorList.test.js
@@ -1,14 +1,34 @@
-import React from 'react';
-import MutationObserver from 'mutationobserver-shim';
+import React from "react";
+import MutationObserver from "mutationobserver-shim";
 
-import { render, screen} from "@testing-library/react";
-import ColorList from './ColorList';
+import { render, screen } from "@testing-library/react";
+import ColorList from "./ColorList";
 
 test("Renders an empty list of colors without errors", () => {
+	render(<ColorList colors={[]} />);
 });
 
-test("Renders a list of colors without errors", () => {
-});
+const colors = [
+	{
+		color: "bisque",
+		code: {
+			hex: "#dd9a99",
+		},
+		id: 7,
+	},
+];
 
-test("Renders the EditForm when editing = true and does not render EditForm when editing = false", () => {
-});
+test("Renders a list of colors without errors", () => {});
+render(<ColorList colors={colors} />);
+
+test("Renders the EditForm when editing = true and does not render EditForm when editing = false", () => {});
+const { rerender } = render(<ColorList colors={colors} editing={true} />);
+
+// let editing = screen.queryByTestId("edit_menu");
+
+expect(screen.queryByTestId("edit_menu")).toBeInTheDocument;
+
+rerender(<ColorList colors={colors} editing={false} />);
+
+expect(screen.queryByTestId("edit_menu")).not.toBeInTheDocument;
+// console.log("after rerender", editing);
